830367e692536df0f2986dfd6c21a60c16afef1a
Jeffrey Lim <jeff@lim.au>
2023-03-31 19:55:25 +0800

    Added mechanism to upload to flash from the console

--- flash.cc
+++ flash.cc
@@ -1,28 +1,27 @@
 //---------------------------------------------------------------------------
 
 #include "flash.h"
+#include "base64.h"
 #include "console.h"
 #include <assert.h>
 #include <string.h>
 
 //---------------------------------------------------------------------------
 
-uint32_t Flash::erasedBytes = 0;
-uint32_t Flash::programmedBytes = 0;
-uint32_t Flash::reprogrammedBytes = 0;
+Flash Flash::instance;
 
 //---------------------------------------------------------------------------
 
 __attribute((weak)) void Flash::Erase(const void *target, size_t size) {
-  erasedBytes += size;
+  instance.erasedBytes += size;
   assert((size & (BLOCK_SIZE - 1)) == 0);
   memset((void *)target, 0xff, size);
 }
 
 __attribute((weak)) void Flash::Write(const void *target, const void *data,
                                       size_t size) {
-  erasedBytes += size;
-  programmedBytes += size;
+  instance.erasedBytes += size;
+  instance.programmedBytes += size;
   assert(target != data);
   assert((size & (BLOCK_SIZE - 1)) == 0);
   memcpy((void *)target, data, size);
@@ -65,11 +64,120 @@ bool Flash::RequiresProgram(const void *target, const void *data, size_t size) {
 
 //---------------------------------------------------------------------------
 
+void Flash::BeginWrite(const uint8_t *address) {
+  target = address;
+
+  const void *baseAddress =
+      (const void *)(intptr_t(address) & -WRITE_DATA_BUFFER_SIZE);
+  size_t offsetIntoPage = size_t(address) & (WRITE_DATA_BUFFER_SIZE - 1);
+  memcpy(buffer, baseAddress, offsetIntoPage);
+}
+
+void Flash::AddData(const uint8_t *data, size_t length) {
+  size_t offset = intptr_t(target) & (WRITE_DATA_BUFFER_SIZE - 1);
+  if (offset + length >= WRITE_DATA_BUFFER_SIZE) {
+    size_t remainingBufferLength = WRITE_DATA_BUFFER_SIZE - offset;
+    memcpy(buffer + offset, data, remainingBufferLength);
+
+    const void *writeAddress =
+        (const void *)(size_t(target) & -WRITE_DATA_BUFFER_SIZE);
+    Flash::Write(writeAddress, buffer, WRITE_DATA_BUFFER_SIZE);
+
+    target += remainingBufferLength;
+    length -= remainingBufferLength;
+    data += remainingBufferLength;
+    offset = 0;
+  }
+  memcpy(buffer + offset, data, length);
+  target += length;
+}
+
+void Flash::WriteRemaining() {
+  size_t bytesToWrite = size_t(target) & (WRITE_DATA_BUFFER_SIZE - 1);
+  if (bytesToWrite != 0) {
+    // Copy the remainder
+    size_t bytesToCopy = WRITE_DATA_BUFFER_SIZE - bytesToWrite;
+    memcpy(buffer + bytesToWrite, target, bytesToCopy);
+
+    const void *writeAddress =
+        (const void *)(size_t(target) & -WRITE_DATA_BUFFER_SIZE);
+
+    Flash::Write(writeAddress, buffer, WRITE_DATA_BUFFER_SIZE);
+  }
+  target = nullptr;
+}
+
+static int HexValue(int c) {
+  if ('0' <= c && c <= '9') {
+    return c - '0';
+  }
+  if ('a' <= c && c <= 'f') {
+    return c - 'a' + 10;
+  }
+  if ('A' <= c && c <= 'F') {
+    return c - 'A' + 10;
+  }
+
+  return -1;
+}
+
+void Flash::BeginWriteBinding(void *context, const char *commandLine) {
+  const char *p = strchr(commandLine, ' ');
+  if (!p) {
+    Console::Printf("ERR Missing address\n\n");
+    return;
+  }
+
+  if (instance.target != nullptr) {
+    Console::Printf("ERR Write in progress\n\n");
+  }
+
+  size_t addressValue = 0;
+  while (*p) {
+    int hexValue = HexValue(*p++);
+    if (hexValue == -1) {
+      continue;
+    }
+    addressValue = 16 * addressValue + hexValue;
+  }
+
+  instance.BeginWrite((const uint8_t *)addressValue);
+
+  Console::SendOk();
+}
+
+void Flash::WriteBinding(void *context, const char *commandLine) {
+  const char *p = strchr(commandLine, ' ');
+  if (!p) {
+    Console::Printf("ERR Missing data\n\n");
+    return;
+  }
+
+  uint8_t decodeBuffer[256];
+  size_t byteCount = Base64::Decode(decodeBuffer, (const uint8_t *)p);
+
+  if (byteCount == 0) {
+    Console::Printf("ERR No data\n\n");
+    return;
+  }
+
+  instance.AddData(decodeBuffer, byteCount);
+
+  Console::SendOk();
+}
+
+void Flash::EndWriteBinding(void *context, const char *commandLine) {
+  instance.WriteRemaining();
+  Console::SendOk();
+}
+
+//---------------------------------------------------------------------------
+
 void Flash::PrintInfo() {
   Console::Printf("Flash session statistics\n");
-  Console::Printf("  Erased bytes: %u\n", Flash::erasedBytes);
-  Console::Printf("  Programmed bytes: %u\n", Flash::programmedBytes);
-  Console::Printf("  Reprogrammed bytes: %u\n", Flash::reprogrammedBytes);
+  Console::Printf("  Erased bytes: %u\n", instance.erasedBytes);
+  Console::Printf("  Programmed bytes: %u\n", instance.programmedBytes);
+  Console::Printf("  Reprogrammed bytes: %u\n", instance.reprogrammedBytes);
 }
 
 //---------------------------------------------------------------------------
--- flash.h
+++ flash.h
@@ -8,6 +8,8 @@
 
 class Flash {
 public:
+  static bool IsUpdating() { return instance.target != nullptr; }
+
   // Sets all bits in the region to 1.
   static void Erase(const void *target, size_t size);
 
@@ -17,15 +19,30 @@ public:
 
   static void PrintInfo();
 
-  static uint32_t erasedBytes;
-  static uint32_t programmedBytes;
-  static uint32_t reprogrammedBytes;
+  static void BeginWriteBinding(void *context, const char *commandLine);
+  static void WriteBinding(void *context, const char *commandLine);
+  static void EndWriteBinding(void *context, const char *commandLine);
 
 private:
   static bool RequiresErase(const void *target, size_t size);
   static bool RequiresErase(const void *target, const void *data, size_t size);
   static bool RequiresProgram(const void *target, const void *data,
                               size_t size);
+
+  void BeginWrite(const uint8_t *address);
+  void AddData(const uint8_t *data, size_t length);
+  void WriteRemaining();
+
+  static const size_t WRITE_DATA_BUFFER_SIZE = 4096;
+
+  uint32_t erasedBytes;
+  uint32_t programmedBytes;
+  uint32_t reprogrammedBytes;
+
+  const uint8_t *target;
+  uint8_t buffer[WRITE_DATA_BUFFER_SIZE];
+
+  static Flash instance;
 };
 
 //---------------------------------------------------------------------------
