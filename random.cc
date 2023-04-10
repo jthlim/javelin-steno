a28a1fec7351532edd708a2775d7309af98142d2
Jeffrey Lim <jeff@lim.au>
2023-04-10 19:28:09 +0800

    Split hardware random from stdlib random

--- random.cc
+++ random.cc
@@ -5,8 +5,12 @@
 
 //---------------------------------------------------------------------------
 
-__attribute__((weak)) uint32_t Random::GenerateUint32() {
+uint32_t Random::GenerateUint32() {
   return rand() ^ (rand() << 10) ^ (rand() << 20);
+}
+
+__attribute__((weak)) uint32_t Random::GenerateHardwareUint32() {
+  return GenerateUint32();
 }
 
 //---------------------------------------------------------------------------
--- random.h
+++ random.h
@@ -8,6 +8,9 @@
 class Random {
 public:
   static uint32_t GenerateUint32();
+
+  // Falls back to GenerateUint32 if no hardware version is available.
+  static uint32_t GenerateHardwareUint32();
 };
 
 //---------------------------------------------------------------------------
