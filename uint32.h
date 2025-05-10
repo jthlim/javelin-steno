5464ff81e96c747c7a01434721fec25250aa3c09
Jeffrey Lim <jeff@lim.au>
2025-05-10 16:25:32 +0800

    [javelin-steno] First working infrared code; able to control Sony & Samsung TVs

---
+++ uint32.h
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#pragma once
+#include <stdint.h>
+
+//---------------------------------------------------------------------------
+
+struct Uint32 {
+  Uint32() = default;
+
+  constexpr Uint32(uint32_t value)
+      : b{(uint8_t)(value), (uint8_t)(value >> 8), (uint8_t)(value >> 16),
+          (uint8_t)(value >> 24)} {}
+
+  uint32_t ToUint32() const {
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24);
+  }
+
+  uint8_t b[4];
+};
+
+static_assert(sizeof(Uint32) == 4);
+static_assert(sizeof(Uint32[2]) == 8);
+
+//---------------------------------------------------------------------------
