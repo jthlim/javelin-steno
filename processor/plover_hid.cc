2dffbf6713697f7b8e0d0325a85278963bf6087f
Jeffrey Lim <jeff@lim.com.au>
2025-12-13 11:18:00 +1030

    Update Plover HID bits to match Plover 5

--- processor/plover_hid.cc
+++ processor/plover_hid.cc
@@ -7,7 +7,7 @@
 
 constexpr uint8_t PLOVER_HID_LOOKUP[] = {
     7,  // S1
-    7,  // S2
+    16, // S2
     6,  // TL
     5,  // KL
     4,  // PL
@@ -17,9 +17,9 @@ constexpr uint8_t PLOVER_HID_LOOKUP[] = {
     0,  // A
     15, // O
     14, // STAR1
-    14, // STAR2
-    14, // STAR3
-    14, // STAR4
+    31, // STAR2
+    30, // STAR3
+    29, // STAR4
     13, // E
     12, // U
     11, // FR
@@ -33,43 +33,43 @@ constexpr uint8_t PLOVER_HID_LOOKUP[] = {
     19, // DR
     18, // ZR
     17, // NUM1
-    17, // NUM2
-    17, // NUM3
-    17, // NUM4
-    17, // NUM5
-    17, // NUM6
-    17, // NUM7
-    17, // NUM8
-    17, // NUM9
-    17, // NUM10
-    17, // NUM11
-    17, // NUM12
-    16, // FUNCTION, ^ -> X1
-    31, // POWER, + -> X2
-    30, // RES1 -> X3
-    29, // RES2 -> X4
-    28, // X5
-    27, // X6
-    26, // X7
-    25, // X8
-    24, // X9
-    39, // X10
-    38, // X11
-    37, // X12
-    36, // X13
-    35, // X14
-    34, // X15
-    33, // X16
-    32, // X17
-    47, // X18
-    46, // X19
-    45, // X20
-    44, // X21
-    43, // X22
-    42, // X23
-    41, // X24
-    40, // X25
-    55, // X26
+    28, // NUM2
+    27, // NUM3
+    26, // NUM4
+    25, // NUM5
+    24, // NUM6
+    39, // NUM7
+    38, // NUM8
+    37, // NUM9
+    36, // NUM10
+    35, // NUM11
+    34, // NUM12
+    33, // FUNCTION, ^ -> X1
+    32, // POWER, + -> X2
+    47, // RES1 -> X3
+    46, // RES2 -> X4
+    45, // X5
+    44, // X6
+    43, // X7
+    42, // X8
+    41, // X9
+    40, // X10
+    55, // X11
+    54, // X12
+    53, // X13
+    52, // X14
+    51, // X15
+    50, // X16
+    49, // X17
+    48, // X18
+    63, // X19
+    62, // X20
+    61, // X21
+    60, // X22
+    59, // X23
+    58, // X24
+    57, // X25
+    56, // X26
 };
 
 static_assert(sizeof(PLOVER_HID_LOOKUP) == (int)StenoKey::COUNT,
